c74e2edc403ecb82252a30396b4ee678f974cf61
camila-painefilo <camila.painefilo@gmail.com>
2025-06-10 13:18:40 +0900

    Update IBA-DCX_Analytics_2.0.py

--- IBA-DCX_Analytics_2.0.py
+++ IBA-DCX_Analytics_2.0.py
@@ -170,7 +170,7 @@ TRANSLATIONS = {
     "✅ Please select a feature":
         {"English": "✅ Please select a feature", "Español": "✅ Selecciona una función"},
     "✅Region/Store Selection Finalized":
-        {"English": "✅ Region/Store has been selected", "Español": "✅ Selección de Región/Local Confirmada"},
+        {"English": "✅ Region/Store has been selected", "Español": "✅ Selección de Región/Negocio Confirmada"},
     "⚠️ Please select the region and store first, then press 'Confirm' to activate the functions.":
         {"English": "⚠️ Please select the region and store first, then press 'Confirm' to activate the functions.",
          "Español": "⚠️ Selecciona primero la región y el negocio y luego pulsa 'Confirmar' para activar las funciones."},
