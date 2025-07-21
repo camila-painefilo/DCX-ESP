9e13561ac9c866322ad6d739c5805517b5c0bbd6
camila-painefilo <camila.painefilo@gmail.com>
2025-07-21 13:14:59 +0900

    Update IBA-DCX_Analytics_2.0.py

--- IBA-DCX_Analytics_2.0.py
+++ IBA-DCX_Analytics_2.0.py
@@ -323,7 +323,7 @@ stopwords = {
     'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
     'you', 'your', 'yours', 'yourself', 'yourselves',
     'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself',
-    'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves','it','s','I','The',
+    'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves','it','s','I','The',"it's",
 
     # 🔹 Question and demonstrative words
     'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those',
